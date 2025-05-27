894448129a1c8c8529bc46cf582755b64d030724
Jacob Berger <jacob@bergerj.com>
2025-05-26 22:07:49 -0700

    alacritty setup and startup script with tmux

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ~/.config/alacritty/start_tmux.sh
+
+# Check if a tmux session exists.
+# We redirect stderr to /dev/null to suppress "no server running on ..." messages if no session exists.
+if tmux has-session 2>/dev/null; then
+  # If a session exists, attach to it.
+  # 'tmux attach' will connect to the last used session or the first available one.
+  exec tmux attach
+else
+  # No session exists. Start a new tmux session.
+  # If you have tmux-resurrect and/or tmux-continuum configured in your ~/.tmux.conf
+  # to automatically restore sessions on server start, that behavior will kick in here.
+  exec tmux
+fi
