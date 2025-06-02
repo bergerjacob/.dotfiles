c0721081832578e8c09e24ee9604e1686f612a97
Jacob Berger <jacob@bergerj.com>
2025-06-02 01:41:45 -0700

    keymaps <leader>{cc,cd,cf}

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -36,8 +36,80 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 -- Show diagnostic hover (float) for the current cursor position
 vim.keymap.set("n", "<leader>k", vim.diagnostic.open_float, { desc = "Show Line diagnostics" })
 
--- Copy full file path of currently opened buffer
-vim.keymap.set('n', '<leader>cp', function()
-  vim.fn.setreg('+', vim.fn.expand('%:p'))
-  print('Copied full path to clipboard: ' .. vim.fn.expand('%:p'))
-end, { noremap = true, silent = false, desc = "Copy full file path to system clipboard" })
+
+
+-- [C]opy full [F]ile path to system clipboard
+vim.keymap.set('n', '<leader>cf', function()
+  local path_to_copy
+  if vim.bo.filetype == 'netrw' then
+    -- In Netrw, get the path of the file/directory under the cursor
+    path_to_copy = vim.fn.expand('%:p:h') .. '/' .. vim.fn.getline('.')
+    -- Basic cleanup for netrw paths (might need more robust parsing for complex cases)
+    path_to_copy = path_to_copy:match("^(.-)@-$") or path_to_copy -- remove trailing @- for symlinks
+    path_to_copy = path_to_copy:match("^(.-)/$") or path_to_copy -- remove trailing / for directories
+    if vim.fn.getline('.'):match("^\"") or vim.fn.getline('.'):match("^=") or vim.fn.getline('.'):match("^Netrw .") then
+        path_to_copy = vim.fn.expand('%:p') -- fallback to current netrw directory
+    else
+        local current_netrw_dir = vim.fn.expand('%:p')
+        if current_netrw_dir:sub(-1) ~= '/' then
+            current_netrw_dir = current_netrw_dir .. '/'
+        end
+        local item_name = vim.fn.getline('.')
+        item_name = item_name:gsub("%s*%.%-%.*$", "")
+        item_name = item_name:gsub("^%s*", "")
+        item_name = item_name:gsub("%s*$", "")
+        item_name = item_name:gsub("[@*/]$", "")
+        path_to_copy = current_netrw_dir .. item_name
+    end
+  else
+    path_to_copy = vim.fn.expand('%:p')
+  end
+  if path_to_copy and #path_to_copy > 0 then
+    vim.fn.setreg('+', path_to_copy)
+    vim.notify('Copied full path to clipboard: ' .. path_to_copy, vim.log.levels.INFO)
+  else
+    vim.notify('No path to copy.', vim.log.levels.WARN)
+  end
+end, { noremap = true, silent = false, desc = "[C]opy full [F]ile path to system clipboard" })
+
+-- [C]opy current [D]irectory to system clipboard
+vim.keymap.set('n', '<leader>cd', function()
+  local dir_to_copy
+  if vim.bo.filetype == 'netrw' then
+    dir_to_copy = vim.fn.expand('%:p')
+    if vim.fn.isdirectory(dir_to_copy) ~= 1 then
+        dir_to_copy = vim.fn.expand('%:p:h')
+    end
+  else
+    dir_to_copy = vim.fn.expand('%:p:h')
+  end
+  if dir_to_copy and #dir_to_copy > 0 then
+    vim.fn.setreg('+', dir_to_copy)
+    vim.notify('Copied directory to clipboard: ' .. dir_to_copy, vim.log.levels.INFO)
+  else
+    vim.notify('No directory to copy.', vim.log.levels.WARN)
+  end
+end, { noremap = true, silent = false, desc = "[C]opy current [D]irectory to system clipboard" })
+
+-- [C]opy current file/buffer [C]ontents to system clipboard
+vim.keymap.set('n', '<leader>cc', function()
+  local current_pos = vim.fn.getpos('.') -- Save cursor position
+  local current_win = vim.fn.win_getid() -- Save current window
+  local message
+
+  vim.cmd('normal! ggVG"+y')        -- Select all and yank to system clipboard
+  vim.fn.win_gotoid(current_win)    -- Restore window focus
+  vim.fn.setpos('.', current_pos)   -- Restore cursor position
+
+  if vim.bo.filetype == 'netrw' then
+    message = 'Copied Netrw listing to clipboard'
+  else
+    local filename = vim.fn.expand('%:t')
+    if filename and #filename > 0 then
+      message = 'Copied contents of ' .. filename .. ' to clipboard'
+    else
+      message = 'Copied buffer contents to clipboard'
+    end
+  end
+  vim.notify(message, vim.log.levels.INFO)
+end, { noremap = true, silent = true, desc = "[C]opy current file/buffer [C]ontents to system clipboard" })
