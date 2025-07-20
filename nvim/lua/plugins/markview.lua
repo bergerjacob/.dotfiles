9af910030a0a0192e87e238526c4ba078d4a3d37
Jacob Berger <jacob@bergerj.com>
2025-07-19 18:36:44 -0700

    fix nvim markview and treesitter load order

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -2,7 +2,6 @@ return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
@@ -15,6 +14,9 @@ return {
 				h5 = 'font-size: 1.1em; font-weight: bold;',
 				h6 = 'font-size: 1.0em; font-weight: bold;',
 			},
+			experimental = {
+				check_rtp_message = false
+			}
 		})
 	end,
 }
