ae93e4d85b9ca7e37d2aa68dcdced442fbb2be1f
Jacob Berger <jacob@bergerj.com>
2026-05-19 21:05:02 -0700

    markview config

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,22 +1,24 @@
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require('markview').setup({
-			custom_css = {
-				h1 = 'font-size: 5em; font-weight: bold; color: red;',
-				h2 = 'font-size: 1.4em; font-weight: bold; color: red;',
-				h3 = 'font-size: 1.3em; font-weight: bold;',
-				h4 = 'font-size: 1.2em; font-weight: bold;',
-				h5 = 'font-size: 1.1em; font-weight: bold;',
-				h6 = 'font-size: 1.0em; font-weight: bold;',
+	opts = function()
+		local presets = require("markview.presets")
+
+		return {
+			preview = {
+				enable_hybrid_mode = true,
+				hybrid_modes = { "n" },
+				linewise_hybrid_mode = true,
+				debounce = 150,
+				draw_range = { 30, 30 },
+				edit_range = { 3, 3 },
 			},
-			experimental = {
-				check_rtp_message = false
-			}
-		})
+			markdown = {
+				headings = presets.headings.glow,
+			},
+			markdown_inline = {
+				checkboxes = presets.checkboxes.nerd,
+			},
+		}
 	end,
 }
